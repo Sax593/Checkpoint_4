657575613a578c9c7f6377dfc9a705f061bab35d
sax59 <waquetv@gmail.com>
2023-02-01 14:15:23 +0100

    Add router in main and route in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "@pages/Home/Home";
 import { Reset } from "styled-reset";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Reset />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
